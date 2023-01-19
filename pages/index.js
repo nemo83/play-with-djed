9649e57c3a1d171eaa675b33106a25a02cc4fe18
Giovanni Gargiulo <gargiulo.gianni@gmail.com>
2023-01-19 06:35:32 +0000

    Initial work

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,105 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
+import { useEffect, useState } from "react"
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+
+  const [djedAmount, setDjedAmount] = useState(0)
+  const [shenAmount, setShenAmount] = useState(0)
+  const [adaUsdRate, setAdaUsdRate] = useState(1.0)
+  const [adaReserveAmt, setAdaReserveAmt] = useState(0)
+  const [collateralPct, setCollateralPct] = useState(0.0)
+
+  // useEffect(() => {
+
+  //   let newCollateralPct
+
+  //   if (djedAmount > 0) {
+
+  //     const totalReserveUSD = adaReserveAmt * adaUsdRate
+  //     newCollateralPct = totalReserveUSD / djedAmount
+
+  //   } else {
+  //     newCollateralPct = 0.0
+  //   }
+
+  //   setCollateralPct(newCollateralPct)
+
+
+  // }, [djedAmount, shenAmount, adaUsdRate, collateralPct])
+  // useEffect(() => {
+
+  //   let newCollateralPct
+
+  //   if (djedAmount > 0) {
+
+  //     const totalReserveUSD = adaReserveAmt * adaUsdRate
+  //     newCollateralPct = totalReserveUSD / djedAmount
+
+  //   } else {
+  //     newCollateralPct = 0.0
+  //   }
+
+  //   setCollateralPct(newCollateralPct)
+
+
+  // }, [djedAmount, shenAmount, adaUsdRate, collateralPct])
+
+  async function calCollateralization() {
+
+    let newCollateralPct
+
+    if (djedAmount > 0) {
+      console.log(' >0')
+
+      const totalReserveUSD = adaReserveAmt * adaUsdRate
+      console.log('totalReserveUSD: ' + totalReserveUSD)
+      newCollateralPct = totalReserveUSD / djedAmount
+
+    } else {
+      console.log('else')
+      newCollateralPct = 0.0
+    }
+
+    setCollateralPct(newCollateralPct)
+  }
+
+  async function calcApproxShenPrice() {
+    if (shenAmount > 0) {
+      return adaReserveAmt * adaUsdRate / shenAmount
+    } else {
+      return 1.0
+    }
+  }
+
+  async function mintShen(shenAmountToMint) {
+    const totalShenAmount = shenAmount + shenAmountToMint
+    const approxShenPrice = calcApproxShenPrice()
+
+    const adaAmtIncres = approxShenPrice * shenAmountToMint
+
+    setShenAmount(totalShenAmount)
+    setAdaReserveAmt(adaAmtIncres)
+    calCollateralization()
+  }
+
+  async function mintOneShen() {
+    return mintShen(1)
+  }
+
+  async function mintDjed(djedAmountToMint) {
+    const totalDjedAmount = djedAmount + djedAmountToMint
+    setDjedAmount(totalDjedAmount)
+    calCollateralization()
+  }
+
+  async function mintOneDjed() {
+    return mintDjed(1)
+  }
+
   return (
     <>
       <Head>
@@ -15,108 +110,25 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.js</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
+        <p>
+          djedAmount: {djedAmount}
+        </p>
+        <p>
+          shenAmount: {shenAmount}
+        </p>
+        <p>
+          collateralPct: {collateralPct}
+        </p>
+        <p>
+          adaUsdRate: {adaUsdRate}
+        </p>
+        <button type="button" onClick={() => { mintOneShen() }} >
+          Mint 1 Shen
+        </button>
+        <button type="button" onClick={() => { mintOneDjed() }} >
+          Mint 1 Djed
+        </button>
 
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
       </main>
     </>
   )
