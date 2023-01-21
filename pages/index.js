7b8d7d1c0e3504337a2874c9bd46a96bfe0b400d
Giovanni Gargiulo <gargiulo.gianni@gmail.com>
2023-01-21 06:53:37 +0000

    '

--- pages/index.js
+++ pages/index.js
@@ -379,9 +379,9 @@ export default function Home() {
                       <div className="p-5 text-black">
                         <div className='m-2'>
                           <h2 className='font-medium text-xl'>What is Djed</h2>
-                          <p>Cardano&aposs native overcollateralized stablecoin, developed by IOG and powered by COTI. $DJED is backed by $ADA and uses $SHEN as a reserve coin.</p>
+                          <p>Cardano's native overcollateralized stablecoin, developed by IOG and powered by COTI. $DJED is backed by $ADA and uses $SHEN as a reserve coin.</p>
                           <p>
-                            To ensure Djed&aposs stability, it uses a collateral ratio between 400% and 800% for $DJED and $SHEN.
+                            To ensure Djed's stability, it uses a collateral ratio between 400% and 800% for $DJED and $SHEN.
                           </p>
                           <p>
                             Find out more on <Link href="https://djed.xyz" className='text-blue-600 hover:text-blue-700 transition duration-300 ease-in-out mb-4'>djed.xyz</Link>
